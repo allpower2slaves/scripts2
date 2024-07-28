59a595233e4a9db084a1d4059b95383d345e11a3
herzeleid02 <81138645+herzeleid02@users.noreply.github.com>
2024-07-28 22:46:53 +0300

    added gnome-setup.sh

---
+++ gnome-setup.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+# ⠈⠙⠒⢦⣄⣸⣿⣿⠟⠉⠀⠀⠀⠀⠀⡀⠀⠀⡠⠀⢀⡄⠀⠀⠀⠙⢿⣤⡀⠀⠈⠙⢦⡀⠈⠙⣦⠀⠀⠁⠲⠿⣦⣤⣶⣷⢀⠀⠀⢀
+# ⠚⠉⣉⣭⣿⡟⠛⠁⠀⢀⡀⠀⠀⣠⡞⠃⢀⠞⠁⠀⣼⠁⠀⢰⠀⠀⡌⠁⠈⢦⡀⠀⠀⠙⢆⠀⠈⢧⠀⠀⠀⢤⡈⠻⣿⠿⣶⣇⣀⡀
+# ⣴⣿⠟⣹⣾⡏⠀⢠⠂⡤⠀⣠⠾⠋⠀⠀⣱⠂⠀⣰⣿⠀⢸⠈⡆⠀⠸⣆⠀⠀⢻⡄⠀⠰⡄⠁⠀⠈⣇⠀⠀⠀⢳⡀⠈⠻⡟⠷⣤⣌
+# ⠟⢡⡾⢣⡿⠁⠀⡎⡼⠀⢠⠁⢀⠄⢀⣾⠃⠀⡼⠋⢻⠀⠘⡀⢡⠀⠀⢹⣆⠀⠀⢿⣆⠀⠹⣆⣆⠀⢸⡀⠀⠀⠀⢷⡀⠀⠘⣆⠘⢧
+# ⠀⡽⢁⣿⠁⠀⢰⢃⠇⠀⡎⢀⠎⠀⢸⡏⠀⡜⠁⠀⢸⡀⠀⣇⠈⠂⠀⠀⣿⡆⠀⢸⠙⢦⡀⢻⣿⡆⠈⡇⠀⢠⠀⠸⡷⡄⠀⠘⡄⠀
+# ⡼⠁⠈⣿⠀⢀⣿⢸⠀⢰⡇⡸⠀⠀⣾⠀⡜⠀⠀⠀⠀⡇⠀⡿⡄⠀⠀⠀⢸⡹⣄⠀⡇⠀⠱⣄⣿⣿⡀⡇⠀⠈⣇⠀⣧⠘⢆⠀⢙⠀
+# ⠁⠀⠀⡇⢀⣾⡟⣰⢀⡟⡇⡇⠀⠀⣧⣼⣀⣀⣠⣄⣄⣼⡀⡇⢱⡀⠀⠀⠈⡿⣿⠶⡧⣄⣀⣸⣿⣽⣿⡇⠀⠀⢻⡀⢸⡆⠈⢧⠸⠀
+# ⠀⠀⠀⣇⡼⢸⣿⣿⣸⢤⣿⠁⡤⣾⣿⠋⠁⣀⣤⢤⣽⡲⢷⢸⠀⢳⡀⠀⠀⢱⣸⣄⣧⣤⣄⡈⠉⢿⣿⢧⠀⠀⢸⡇⢸⣿⠀⠀⢷⠂
+# ⠀⠀⠀⢹⠁⡞⢈⣿⡿⢮⣻⠀⡴⠡⢄⣴⣿⣯⣼⠀⠀⠉⠀⢿⡆⠀⠱⡀⠀⠸⣷⣿⣿⣤⡏⠙⣦⣘⠷⠺⡆⠀⢠⣷⢸⠘⡄⠀⠀⠀
+# ⠀⠀⠀⢈⣀⡇⢸⣏⠃⡖⢿⣴⠁⠀⠘⢿⣻⣿⣿⠀⠀⠀⠀⠀⠁⠀⠀⠱⡄⠀⡟⣿⣿⣿⠃⠀⠸⠃⠀⠀⣿⠀⢸⣿⡟⣟⡇⠀⠀⠀
+# ⠀⠀⠀⢸⢸⠇⢸⠘⢦⡳⢼⣽⡀⠀⠀⠀⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⡇⠀⠈⠁⠀⠀⠀⠀⠀⢰⣿⣇⢸⢿⡆⢿⡇⠀⠀⢀
+# ⠀⠀⠀⢸⣸⠀⢸⠀⠀⠈⢢⠉⢻⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⡾⠛⣹⡞⢈⡇⢸⡇⠀⠀⢸
+# ⠀⠀⠀⠀⣿⠀⢸⠀⠀⠀⢸⠛⠋⢷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣷⢿⠀⠀⢸⣿⢸⡇⠀⠀⢸
+# ⠀⠀⠀⠀⣿⠀⢸⠆⠀⠀⢸⠀⠀⠈⢳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠏⡇⣸⠀⠀⢸⣿⠘⣧⠀⡀⢸
+# ⠀⠀⠀⠀⣿⠀⣿⠀⠀⠀⢸⠀⡀⠀⢠⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠀⠀⠀⠀⠀⣠⣞⠁⡸⠀⣿⡆⠀⠈⣿⣾⣻⠀⣿⠀
+# ⠀⠀⠀⠀⢸⠀⡏⠀⠀⠀⢸⢠⢇⠀⠘⡄⠀⠙⢦⡀⠀⠀⠀⠀⠀⢰⠟⠉⠐⠊⣇⠀⢀⡠⠞⣹⠏⢀⠇⠀⠋⣧⠀⠀⢹⣿⡏⠀⠿⡆
+# ⠀⠀⠀⠀⢸⠀⡇⠀⡤⠀⣼⣼⢸⠀⠀⢿⡀⠀⠀⡟⠷⣤⡀⠀⢀⡎⠀⡄⠀⡀⠸⣴⣿⣀⣠⡏⠀⠘⠀⠀⢸⠸⣄⣀⣀⣿⣧⣀⣠⢳
+# ⠀⠆⠀⠀⢸⣾⠀⡼⠀⠀⣷⠃⢸⠀⠀⠸⣷⡏⢯⡇⠀⠈⠻⣷⡼⠠⠖⠁⠀⠋⠀⣿⣏⡗⠈⠱⣖⢶⢶⠒⣺⡿⢿⢷⡶⠛⠉⠉⠉⠚
+# ⠀⠀⡀⠀⢸⡇⣰⠁⠀⢸⡏⠀⠈⣇⣀⣠⡿⠀⠀⠱⣄⠀⠀⢠⡇⠰⠆⢀⣠⣼⣇⣿⡏⠀⠀⠀⠈⢿⠾⢋⣝⣻⣿⣿⠃⠀⠀⠀⠀⠀
+# ⡀⠀⡇⠀⢸⣧⠃⠀⠀⣾⠁⠀⣀⣿⣿⡿⠀⠀⠀⠀⠈⠣⣀⣼⣕⣤⣤⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠘⣯⣡⣏⣽⣻⡿⠀⠀⠀⠀⠀⠀
+# ⠃⠀⠃⠀⣾⠃⠀⣀⣼⣷⠒⢻⣛⣿⣷⣧⠀⠀⠀⠀⠀⠀⣸⠉⠉⠉⠉⠉⠙⠋⠉⠉⠙⡇⠀⠀⠀⠀⢹⢭⣼⣾⣻⡇⠀⠀⠀⠀⠀⠀
+# ⠀⢸⠀⣸⣥⡖⣿⠿⣿⣿⢻⣟⠻⣏⣭⣿⠀⠀⠀⠀⠀⠀⠙⣶⣦⠤⣤⣤⢤⡤⣤⠖⠚⠁⠀⠀⠀⠀⠈⣏⣬⣹⣾⠁⠀⠀⠀⠀⠀⠀
+# ⠀⣠⠔⠋⠹⣯⠟⣷⢈⣭⠗⣦⡷⣎⣙⣻⠀⠀⠀⠀⠀⠀⣼⣿⣿⡞⠁⢸⢀⣿⠽⠗⠲⡀⠀⠀⠀⠀⠀⢿⡞⣿⣿⡄⠀⠀⠀⠀⠀⠀
+# ⡞⠁⠀⠀⠀⠘⣿⣱⣯⢀⡼⠟⣤⢞⣺⣿⡄⠀⠀⠀⢀⣼⣿⠿⢋⡠⠔⠋⠁⠀⠀⠀⠀⢻⣤⡀⠀⠀⠀⢸⣿⡟⣿⡇⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠘⣿⣇⠨⣡⢹⣻⣄⣿⣮⡇⠀⣀⣴⣿⠟⠉⠉⠉⠐⠂⠐⠒⠒⠒⠒⠒⠸⢯⣟⣲⣄⡀⢸⣿⡽⣿⡇⠀⠀⠀⠀⠀⠀
+
+# this is my personal script for setting up gnome
+
+# mouse
+gsettings set org.gnome.desktop.peripherals.mouse speed 0.38
+gsettings set org.gnome.desktop.peripherals.mouse accel-profile 'flat'
+gsettings set org.gnome.shell.app-switcher current-workspace-only true
+#TODO: add in trackpoint accel turn off
+
+# nautilus
+gsettings set org.gnome.nautilus.preferences search-filter-time-type 'last_modified'
+gsettings set org.gnome.nautilus.preferences default-sort-in-reverse-order true
+gsettings set org.gnome.nautilus.preferences default-sort-order 'mtime'
+gsettings set org.gnome.nautilus.preferences search-filter-time-type 'last_modified'
+
+# mutter wm
+gsettings set org.gnome.mutter attach-modal-dialogs false
+gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
+gsettings set org.gnome.desktop.wm.preferences focus-mode 'click'
+
+# desktop settings
+gsettings set org.gnome.desktop.desktop clock-format '24h'
+gsettings set org.gnome.shell.app-switcher current-workspace-only 'true'
+
+# keyboard input settings
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'ru')]"
+gsettings set org.gnome.desktop.input-sources mru-sources "[('xkb', 'us'), ('xkb', 'ru')]"
+# maybe i should make these two share the same variable... idk if i even need mru thing
+gsettings set org.gnome.desktop.input-sources xkb-options "['caps:none']"
+
+# keyboard keybinds
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications []
+
+# keyboard keybinds part ii
+# TODO: rewrite this thing to be more elegant (maybe utilize a var?)
+gsettings get org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'nautilus'
+gsettings get org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'nautilus'
+gsettings get org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Super>e'
+
+gsettings get org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name 'konsole'
+gsettings get org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command 'flatpak run org.kde.konsole'
+gsettings get org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding '<Control><Alt>t'
